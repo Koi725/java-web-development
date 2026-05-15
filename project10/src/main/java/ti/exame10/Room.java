7a6a687d79773c79fa8dff0e50a96ff5f7aa38e6
Koi725 <kosharezae@yahoo.com>
2026-05-15 17:00:00 +0100

    feat : Room structer file added

---
+++ project10/src/main/java/ti/exame10/Room.java
@@ -0,0 +1,39 @@
+package ti.exame10;
+
+public class Room {
+    private int number ;
+    private String type ;
+    private boolean available ;
+
+    public Room(){};
+
+    public Room(int number, String type, boolean available) {
+        this.number = number;
+        this.type = type;
+        this.available = available;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+     public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+}
